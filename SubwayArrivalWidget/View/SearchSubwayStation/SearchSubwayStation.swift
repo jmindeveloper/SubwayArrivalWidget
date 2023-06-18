ff16d71f3e0d6ac4b780fb04f03cd2f5d90d4252
jmindeveloper <jmin.developer@gmail.com>
2023-06-18 20:41:50 +0900

    검색 텍스트필드

--- SubwayArrivalWidget/View/SearchSubwayStation/SearchSubwayStation.swift
+++ SubwayArrivalWidget/View/SearchSubwayStation/SearchSubwayStation.swift
@@ -5,4 +5,29 @@
 //  Created by J_Min on 2023/06/18.
 //
 
-import Foundation
+import SwiftUI
+
+struct SearchSubwayStation<ViewModel>: View where ViewModel: SearchSubwayStationViewModelInterface {
+    
+    @ObservedObject var viewModel: ViewModel
+    @FocusState private var searchBarFocused: Bool
+    
+    var body: some View {
+        VStack {
+            TextField("검색", text: $viewModel.searchSubwayStationName)
+                .textFieldStyle(.roundedBorder)
+                .focused($searchBarFocused)
+                .padding()
+            
+            Spacer()
+        }
+        .onTapGesture {
+            searchBarFocused = false
+        }
+    }
+}
+
+@available(iOS 17, *)
+#Preview {
+    SearchSubwayStation(viewModel: SearchSubwayStationViewModel())
+}
