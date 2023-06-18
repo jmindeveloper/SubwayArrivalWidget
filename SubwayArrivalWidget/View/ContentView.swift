4831ee8c539687f5b034ad3bd21756aa28391d77
jmindeveloper <jmin.developer@gmail.com>
2023-06-18 19:34:17 +0900

    폴더구조 정리

---
+++ SubwayArrivalWidget/View/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SubwayArrivalWidget
+//
+//  Created by J_Min on 2023/06/18.
+//
+
+import SwiftUI
+import Combine
+
+struct ContentView: View {
+    
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
